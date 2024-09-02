fae7b3efd029fed002ff02454733fb31f120b332
akashprinceakash <akashprinceakash9986@gmail.com>
2024-09-02 14:37:24 +0530

    moved Server to repository

---
+++ Server/model/users.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+const Userschema=new mongoose.Schema({
+    name:String,
+    email:String,
+    age:Number
+})
+const UserModel=mongoose.model("users",Userschema);
+module.exports=UserModel
